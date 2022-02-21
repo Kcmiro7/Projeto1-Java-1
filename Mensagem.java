0d6be24cd10e17d9a454fbfd78654bc5d5f8322e
Gustavo Cassemiro Silva <gustavo-santosfc@hotmail.com>
2022-02-21 19:27:29 +0000

    Atualizacao

--- Mensagem.java
+++ Mensagem.java
@@ -1,5 +1,9 @@
 public class Mensagem{
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
+        int idade = 21;
+        System.out.println("Gustavo tem " +idade +" anos ");
+    
         System.out.println("Fatec Pompeia");
     }
 }
